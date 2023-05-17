1d41a36ff71bc49de5f4bf53f73c2ade7edc48b9
Dunia <douniamondesmile@gmail.com>
2023-05-17 10:28:46 +0100

    initial commit

---
+++ src/app/publishers/publishers.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddPublisherComponent } from './components/add-publisher/add-publisher.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddPublisherComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PublishersModule { }
